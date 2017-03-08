d07c5ae7f89cd617562ba4e6d210bfc6d40ae33b
Valters Valdersteins <valters.valdersteins@gmail.com>
2017-03-08 20:51:22 +0200

    Path fixed for chat template url

--- src/components/chat/chat.component.ts
+++ src/components/chat/chat.component.ts
@@ -2,7 +2,7 @@ import { Component,Directive } from '@angular/core';
 
 @Component({
     selector: 'chatbox',
-    templateUrl: 'src/components/chat/chat.component.html',
+    templateUrl: 'chat.component.html',
 })
 export class ChatComponent {
     private message: String;
